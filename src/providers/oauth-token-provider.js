fc44210418af5b7f16f55a3cd931f41be50ee5be
Rui Silva <rui.penso@gmail.com>
2015-02-04 00:26:39 +0000

    Add options to `ipCookie.remove()` on OAuthToken

--- src/providers/oauth-token-provider.js
+++ src/providers/oauth-token-provider.js
@@ -101,7 +101,7 @@ function OAuthTokenProvider() {
        */
 
       removeToken() {
-        return ipCookie.remove(config.name);
+        return ipCookie.remove(config.name, config.options);
       }
     }
 
