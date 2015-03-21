d5669b243020ab336c7d752c794a882ab3fc2593
João Granado <jmg@seegno.com>
2015-03-21 18:05:58 +0000

    Update `angular-cookies.mock`

--- test/mocks/angular-cookies.mock.js
+++ test/mocks/angular-cookies.mock.js
@@ -4,24 +4,20 @@
  */
 
 angular.module('angular-cookies.mock', [])
-  .factory('ipCookie', function() {
-    return (function () {
-      var cookie;
+  .provider('$cookies', function() {
+    this.$get = function() {
+      var cookieStore = {};
 
-      function cookieFun(key, value, options) {
-        if (undefined !== value) {
-          cookie = value;
+      return {
+        getObject: function(key) {
+          return cookieStore[key];
+        },
+        putObject: function(key, value, options) {
+          cookieStore[key] = value;
+        },
+        remove: function(key) {
+          delete cookieStore[key];
         }
-
-        return cookie;
       }
-
-      cookieFun.remove = function(key, options) {
-        cookie = undefined;
-
-        return;
-      };
-
-      return cookieFun;
-    }());
+    }
   });
