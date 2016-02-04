2586f17035e5a7e6f5db2b8efdb8d9836efa14a6
Rui Silva <penso@uphold.com>
2016-02-04 15:31:29 +0000

    Update interceptor to allow authorization header to be overridden

--- src/interceptors/oauth-interceptor.js
+++ src/interceptors/oauth-interceptor.js
@@ -8,9 +8,10 @@
 function oauthInterceptor($q, $rootScope, OAuthToken) {
   return {
     request: function(config) {
+      config.headers = config.headers || {};
+
       // Inject `Authorization` header.
-      if (OAuthToken.getAuthorizationHeader()) {
-        config.headers = config.headers || {};
+      if (!config.headers.hasOwnProperty('Authorization') && OAuthToken.getAuthorizationHeader()) {
         config.headers.Authorization = OAuthToken.getAuthorizationHeader();
       }
 
