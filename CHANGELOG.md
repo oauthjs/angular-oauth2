beb1d54b06a75af3999818848431091fe09c13fe
Rui Silva <penso@uphold.com>
2016-02-12 16:43:03 +0000

    Release 3.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 ## Changelog
 
-### upcoming / 2016-02-10
+### 3.1.1 / 2016-02-12
+- [#79](https://github.com/seegno/angular-oauth2/pull/79) Update dependencies (@ruipenso)
+
+### 3.1.0 / 2016-02-10
 - [#78](https://github.com/seegno/angular-oauth2/pull/78) Update OAuth methods to allow `data` and `options` override (@ruipenso)
 - [#71](https://github.com/seegno/angular-oauth2/pull/71) Add `client_id` and `client_secret` on revoke_token (@tinogomes)
 - [#77](https://github.com/seegno/angular-oauth2/pull/77) Update interceptor to allow authorization header to be overridden (@ruipenso)
--- dist/angular-oauth2.js
+++ dist/angular-oauth2.js
@@ -1,6 +1,6 @@
 /**
  * angular-oauth2 - Angular OAuth2
- * @version v3.1.0
+ * @version v3.1.1
  * @link https://github.com/seegno/angular-oauth2
  * @license MIT
  */
@@ -20,19 +20,19 @@
     oauthConfig.$inject = [ "$httpProvider" ];
     function oauthInterceptor($q, $rootScope, OAuthToken) {
         return {
-            request: function(config) {
-                config.headers = config.headers || {};
-                if (!config.headers.hasOwnProperty("Authorization") && OAuthToken.getAuthorizationHeader()) {
+            request: function request(config) {
+                if (OAuthToken.getAuthorizationHeader()) {
+                    config.headers = config.headers || {};
                     config.headers.Authorization = OAuthToken.getAuthorizationHeader();
                 }
                 return config;
             },
-            responseError: function(rejection) {
+            responseError: function responseError(rejection) {
                 if (400 === rejection.status && rejection.data && ("invalid_request" === rejection.data.error || "invalid_grant" === rejection.data.error)) {
                     OAuthToken.removeToken();
                     $rootScope.$emit("oauth:error", rejection);
                 }
-                if (401 === rejection.status && (rejection.data && "invalid_token" === rejection.data.error) || rejection.headers("www-authenticate") && 0 === rejection.headers("www-authenticate").indexOf("Bearer")) {
+                if (401 === rejection.status && rejection.data && "invalid_token" === rejection.data.error || rejection.headers("www-authenticate") && 0 === rejection.headers("www-authenticate").indexOf("Bearer")) {
                     $rootScope.$emit("oauth:error", rejection);
                 }
                 return $q.reject(rejection);
@@ -40,10 +40,27 @@
         };
     }
     oauthInterceptor.$inject = [ "$q", "$rootScope", "OAuthToken" ];
-    var _prototypeProperties = function(child, staticProps, instanceProps) {
-        if (staticProps) Object.defineProperties(child, staticProps);
-        if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
-    };
+    var _createClass = function() {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ("value" in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
+        return function(Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    }();
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
     var defaults = {
         baseUrl: null,
         clientId: null,
@@ -81,107 +98,112 @@
         this.$get = function($http, OAuthToken) {
             var OAuth = function() {
                 function OAuth() {
+                    _classCallCheck(this, OAuth);
                     if (!config) {
                         throw new Error("`OAuthProvider` must be configured first.");
                     }
                 }
-                _prototypeProperties(OAuth, null, {
-                    isAuthenticated: {
-                        value: function isAuthenticated() {
-                            return !!OAuthToken.getToken();
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    getAccessToken: {
-                        value: function getAccessToken(data, options) {
-                            data = angular.extend({
-                                client_id: config.clientId,
-                                grant_type: "password"
-                            }, data);
-                            if (null !== config.clientSecret) {
-                                data.client_secret = config.clientSecret;
-                            }
-                            data = queryString.stringify(data);
-                            options = angular.extend({
-                                headers: {
-                                    Authorization: undefined,
-                                    "Content-Type": "application/x-www-form-urlencoded"
-                                }
-                            }, options);
-                            return $http.post("" + config.baseUrl + "" + config.grantPath, data, options).then(function(response) {
-                                OAuthToken.setToken(response.data);
-                                return response;
-                            });
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    getRefreshToken: {
-                        value: function getRefreshToken(data, options) {
-                            data = angular.extend({
-                                client_id: config.clientId,
-                                grant_type: "refresh_token",
-                                refresh_token: OAuthToken.getRefreshToken()
-                            }, data);
-                            if (null !== config.clientSecret) {
-                                data.client_secret = config.clientSecret;
-                            }
-                            data = queryString.stringify(data);
-                            options = angular.extend({
-                                headers: {
-                                    Authorization: undefined,
-                                    "Content-Type": "application/x-www-form-urlencoded"
-                                }
-                            }, options);
-                            return $http.post("" + config.baseUrl + "" + config.grantPath, data, options).then(function(response) {
-                                OAuthToken.setToken(response.data);
-                                return response;
-                            });
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    revokeToken: {
-                        value: function revokeToken(data, options) {
-                            var refreshToken = OAuthToken.getRefreshToken();
-                            data = angular.extend({
-                                client_id: config.clientId,
-                                token: refreshToken ? refreshToken : OAuthToken.getAccessToken(),
-                                token_type_hint: refreshToken ? "refresh_token" : "access_token"
-                            }, data);
-                            if (null !== config.clientSecret) {
-                                data.client_secret = config.clientSecret;
-                            }
-                            data = queryString.stringify(data);
-                            options = angular.extend({
-                                headers: {
-                                    "Content-Type": "application/x-www-form-urlencoded"
-                                }
-                            }, options);
-                            return $http.post("" + config.baseUrl + "" + config.revokePath, data, options).then(function(response) {
-                                OAuthToken.removeToken();
-                                return response;
-                            });
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
+                _createClass(OAuth, [ {
+                    key: "isAuthenticated",
+                    value: function isAuthenticated() {
+                        return !!OAuthToken.getToken();
                     }
-                });
+                }, {
+                    key: "getAccessToken",
+                    value: function getAccessToken(data, options) {
+                        data = angular.extend({
+                            client_id: config.clientId,
+                            grant_type: "password"
+                        }, data);
+                        if (null !== config.clientSecret) {
+                            data.client_secret = config.clientSecret;
+                        }
+                        data = queryString.stringify(data);
+                        options = angular.extend({
+                            headers: {
+                                Authorization: undefined,
+                                "Content-Type": "application/x-www-form-urlencoded"
+                            }
+                        }, options);
+                        return $http.post("" + config.baseUrl + config.grantPath, data, options).then(function(response) {
+                            OAuthToken.setToken(response.data);
+                            return response;
+                        });
+                    }
+                }, {
+                    key: "getRefreshToken",
+                    value: function getRefreshToken(data, options) {
+                        data = angular.extend({
+                            client_id: config.clientId,
+                            grant_type: "refresh_token",
+                            refresh_token: OAuthToken.getRefreshToken()
+                        }, data);
+                        if (null !== config.clientSecret) {
+                            data.client_secret = config.clientSecret;
+                        }
+                        data = queryString.stringify(data);
+                        options = angular.extend({
+                            headers: {
+                                Authorization: undefined,
+                                "Content-Type": "application/x-www-form-urlencoded"
+                            }
+                        }, options);
+                        return $http.post("" + config.baseUrl + config.grantPath, data, options).then(function(response) {
+                            OAuthToken.setToken(response.data);
+                            return response;
+                        });
+                    }
+                }, {
+                    key: "revokeToken",
+                    value: function revokeToken(data, options) {
+                        var refreshToken = OAuthToken.getRefreshToken();
+                        data = angular.extend({
+                            client_id: config.clientId,
+                            token: refreshToken ? refreshToken : OAuthToken.getAccessToken(),
+                            token_type_hint: refreshToken ? "refresh_token" : "access_token"
+                        }, data);
+                        if (null !== config.clientSecret) {
+                            data.client_secret = config.clientSecret;
+                        }
+                        data = queryString.stringify(data);
+                        options = angular.extend({
+                            headers: {
+                                "Content-Type": "application/x-www-form-urlencoded"
+                            }
+                        }, options);
+                        return $http.post("" + config.baseUrl + config.revokePath, data, options).then(function(response) {
+                            OAuthToken.removeToken();
+                            return response;
+                        });
+                    }
+                } ]);
                 return OAuth;
             }();
             return new OAuth();
         };
         this.$get.$inject = [ "$http", "OAuthToken" ];
     }
-    var _prototypeProperties = function(child, staticProps, instanceProps) {
-        if (staticProps) Object.defineProperties(child, staticProps);
-        if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
-    };
+    var _createClass = function() {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ("value" in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
+        return function(Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    }();
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
     function OAuthTokenProvider() {
         var config = {
             name: "token",
@@ -198,68 +220,48 @@
         };
         this.$get = function($cookies) {
             var OAuthToken = function() {
-                function OAuthToken() {}
-                _prototypeProperties(OAuthToken, null, {
-                    setToken: {
-                        value: function setToken(data) {
-                            return $cookies.putObject(config.name, data, config.options);
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    getToken: {
-                        value: function getToken() {
-                            return $cookies.getObject(config.name);
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    getAccessToken: {
-                        value: function getAccessToken() {
-                            return this.getToken() ? this.getToken().access_token : undefined;
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    getAuthorizationHeader: {
-                        value: function getAuthorizationHeader() {
-                            if (!(this.getTokenType() && this.getAccessToken())) {
-                                return;
-                            }
-                            return "" + (this.getTokenType().charAt(0).toUpperCase() + this.getTokenType().substr(1)) + " " + this.getAccessToken();
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    getRefreshToken: {
-                        value: function getRefreshToken() {
-                            return this.getToken() ? this.getToken().refresh_token : undefined;
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    getTokenType: {
-                        value: function getTokenType() {
-                            return this.getToken() ? this.getToken().token_type : undefined;
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
-                    },
-                    removeToken: {
-                        value: function removeToken() {
-                            return $cookies.remove(config.name, config.options);
-                        },
-                        writable: true,
-                        enumerable: true,
-                        configurable: true
+                function OAuthToken() {
+                    _classCallCheck(this, OAuthToken);
+                }
+                _createClass(OAuthToken, [ {
+                    key: "setToken",
+                    value: function setToken(data) {
+                        return $cookies.putObject(config.name, data, config.options);
                     }
-                });
+                }, {
+                    key: "getToken",
+                    value: function getToken() {
+                        return $cookies.getObject(config.name);
+                    }
+                }, {
+                    key: "getAccessToken",
+                    value: function getAccessToken() {
+                        return this.getToken() ? this.getToken().access_token : undefined;
+                    }
+                }, {
+                    key: "getAuthorizationHeader",
+                    value: function getAuthorizationHeader() {
+                        if (!(this.getTokenType() && this.getAccessToken())) {
+                            return;
+                        }
+                        return this.getTokenType().charAt(0).toUpperCase() + this.getTokenType().substr(1) + " " + this.getAccessToken();
+                    }
+                }, {
+                    key: "getRefreshToken",
+                    value: function getRefreshToken() {
+                        return this.getToken() ? this.getToken().refresh_token : undefined;
+                    }
+                }, {
+                    key: "getTokenType",
+                    value: function getTokenType() {
+                        return this.getToken() ? this.getToken().token_type : undefined;
+                    }
+                }, {
+                    key: "removeToken",
+                    value: function removeToken() {
+                        return $cookies.remove(config.name, config.options);
+                    }
+                } ]);
                 return OAuthToken;
             }();
             return new OAuthToken();
