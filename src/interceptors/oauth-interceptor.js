1d6034761b6f3dd83dd3787ebd577a41ca956232
Rui Silva <penso@uphold.com>
2016-02-12 14:49:49 +0000

    Revert "Merge pull request #77 from seegno/enhancement/authorization-header"

--- src/interceptors/oauth-interceptor.js
+++ src/interceptors/oauth-interceptor.js
@@ -6,10 +6,9 @@
 function oauthInterceptor($q, $rootScope, OAuthToken) {
   return {
     request: function(config) {
-      config.headers = config.headers || {};
-
       // Inject `Authorization` header.
-      if (!config.headers.hasOwnProperty('Authorization') && OAuthToken.getAuthorizationHeader()) {
+      if (OAuthToken.getAuthorizationHeader()) {
+        config.headers = config.headers || {};
         config.headers.Authorization = OAuthToken.getAuthorizationHeader();
       }
 
